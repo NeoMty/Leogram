51af0f97aa08c36260d360149edd751734cca774
Eduardo Pineda <edpinedaguerra@gmail.com>
2024-05-08 18:32:28 -0600

    feat: Create storage and database design

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,6 +1,6 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, } from "@/components/ui/form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { useForm } from "react-hook-form"
@@ -9,9 +9,11 @@ import { z } from "zod"
 import Loader from "@/components/shared/Loader"
 import { Link } from "react-router-dom"
 import { createUserAccount } from "@/lib/appwrite/api"
+import { useToast } from "@/components/ui/use-toast"
+
 
 const SignupForm = () => {
-
+  const { toast } = useToast()
   const isLoading = false;
 
   // 1. Define your form.
@@ -28,7 +30,11 @@ const SignupForm = () => {
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
     const newUser = await createUserAccount(values);
-    console.log(newUser);
+    if (!newUser) {
+      return toast({title: 'Sign up failed. Please try again.'})
+    }
+
+    //const session = await signInAccount()
   }
 
   return (
