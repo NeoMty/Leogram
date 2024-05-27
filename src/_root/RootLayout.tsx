a064cf8e1a71d2f4a9fe05a44f7938bbe775160b
Eduardo Pineda <edpinedaguerra@gmail.com>
2024-05-26 19:49:38 -0600

    feat: Add navigation bars for mobile and desktop

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import Bottombar from '@/components/shared/Bottombar'
+import Topbar from '@/components/shared/Topbar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+
+import { Outlet } from 'react-router-dom'
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
+      <section className='flex flex-1 h-full'>
+        <Outlet />
+      </section>
+
+      <Bottombar />
+    </div>
   )
 }
 
